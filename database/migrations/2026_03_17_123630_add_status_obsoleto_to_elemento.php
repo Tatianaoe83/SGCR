6cf6b3b2eaf4efeac03c59106de96d59462de05b
EduardoCong2026 <econg@proser.com.mx>
2026-03-17 12:40:44 -0600

    fix(add column obsoleto to enum status)

---
+++ database/migrations/2026_03_17_123630_add_status_obsoleto_to_elemento.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        DB::statement("
+            ALTER TABLE elemento
+            MODIFY COLUMN status ENUM(
+                'En Proceso',
+                'En Firmas',
+                'Publicado',
+                'Rechazado',
+                'Obsoleto'
+            ) NOT NULL DEFAULT 'En Proceso'
+        ");
+    }
+
+    public function down(): void
+    {
+        DB::statement("
+            ALTER TABLE elementos
+            MODIFY COLUMN status ENUM(
+                'En Proceso',
+                'En Firmas',
+                'Publicado',
+                'Rechazado'
+            ) NOT NULL DEFAULT 'En Proceso'
+        ");
+    }
+};
